05e0659cac7cd02f8ba4fdfde497e53dcc8cc64b
Miguel Martinez <miguel.martinez9@utp.ac.pa>
2025-09-17 20:12:26 -0500

    Cargando Lab 1 y Lab 2

---
+++ laboratorio2/laboratorio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Client client = new Client();
+            client.FirstName = "Miguel";
+            client.LastName = "Martinez";
+            client.Age = 25;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+
+        public class Client
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string GetFullName()
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+    }
+
+
