3e402f8423c476572bc9e18d334f4a66f6b10dc1
Miguel Martinez <miguel.martinez9@utp.ac.pa>
2025-10-14 19:19:56 -0500

    Laboratorio 11 completo y subida a Git

---
+++ laboratorio11/laboratorio11/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace laboratorio11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnClickThis = new Button();
+            lblHelloWorld = new Label();
+            SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            btnClickThis.Location = new Point(454, 86);
+            btnClickThis.Name = "btnClickThis";
+            btnClickThis.Size = new Size(112, 34);
+            btnClickThis.TabIndex = 0;
+            btnClickThis.Text = "Click this";
+            btnClickThis.UseVisualStyleBackColor = true;
+            btnClickThis.Click += button1_Click;
+            // 
+            // lblHelloWorld
+            // 
+            lblHelloWorld.AutoSize = true;
+            lblHelloWorld.Location = new Point(479, 137);
+            lblHelloWorld.Name = "lblHelloWorld";
+            lblHelloWorld.Size = new Size(59, 25);
+            lblHelloWorld.TabIndex = 1;
+            lblHelloWorld.Text = "label1";
+            lblHelloWorld.Click += lblHelloWorld_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblHelloWorld);
+            Controls.Add(btnClickThis);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnClickThis;
+        private Label lblHelloWorld;
+    }
+}
